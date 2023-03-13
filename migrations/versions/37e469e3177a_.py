15998b80b0ce185b5746400879570002f711b46b
Fadi <1fadi@proton.me>
2023-03-13 15:34:35 +0100

    initialized alembic migrations

---
+++ migrations/versions/37e469e3177a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 37e469e3177a
+Revises: 5e049134da3b
+Create Date: 2023-03-08 11:19:03.648683
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '37e469e3177a'
+down_revision = '5e049134da3b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
